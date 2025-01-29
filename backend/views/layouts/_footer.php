0848c53cb1548f6282c4f37ada592d850f422bbf
stlmargy <stellahmargy0211@gmail.com>
2025-01-29 15:10:40 +0300

    adds footer to main layout-main layout complete

---
+++ backend/views/layouts/_footer.php
@@ -0,0 +1,32 @@
+<?php
+use yii\web\View ;
+?>
+<footer class="bg-color text-white py-4 mt-5 ">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>About Us</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula nulla ut justo luctus, a aliquam lorem pretium.</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Contact Information</h5>
+                <ul class="list-unstyled">
+                    <li>Email: embassavesacco@gmail.com</li>
+                    <li>Phone: +254719529449</li>
+                    <li>Address: Biashara Street,Nairobi</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h5>Follow Us</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Facebook</a></li>
+                    <li><a href="#" class="text-white">Twitter</a></li>
+                    <li><a href="#" class="text-white">Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="text-center mt-3">
+            <p>&copy; 2025 My Company. All rights reserved.</p>
+        </div>
+    </div>
+</footer>
