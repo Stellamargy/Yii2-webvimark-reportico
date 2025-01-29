c19e988f597fcf2960340f10665d255666ef3fff
stlmargy <stellahmargy0211@gmail.com>
2025-01-29 10:33:05 +0300

    database connection

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -9,7 +9,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <?= getenv('DB_USERNAME'); ?>
     <p>This is the About page. You may modify the following file to customize its content:</p>
 
     <code><?= __FILE__ ?></code>
