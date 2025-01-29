fee95494bbf565a3074e566e2c97b16029d2d80f
stlmargy <stellahmargy0211@gmail.com>
2025-01-29 11:32:25 +0300

    configures backend and frontend to use webvimark module user management

--- backend/config/main.php
+++ backend/config/main.php
@@ -16,10 +16,17 @@ return [
         'request' => [
             'csrfParam' => '_csrf-backend',
         ],
+
+        // I am no longer using the inbuilt user component for usermanagement
+        //I am using the  webvimark/module-user-management for usermanagement
         'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+            'class' => 'webvimark\modules\UserManagement\components\UserConfig',
+
+            // Comment this if you don't want to record user logins
+            'on afterLogin' => function ($event) {
+                \webvimark\modules\UserManagement\models\UserVisitLog::newVisitor($event->identity->id);
+            }
+
         ],
         'session' => [
             // this is the name of the session cookie used for login on the backend
@@ -37,14 +44,43 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+        
     ],
     'params' => $params,
+    'modules'=>[
+	'user-management' => [
+		'class' => 'webvimark\modules\UserManagement\UserManagementModule',
+
+		// 'enableRegistration' => true,
+
+		// Add regexp validation to passwords. Default pattern does not restrict user and can enter any set of characters.
+		// The example below allows user to enter :
+		// any set of characters
+		// (?=\S{8,}): of at least length 8
+		// (?=\S*[a-z]): containing at least one lowercase letter
+		// (?=\S*[A-Z]): and at least one uppercase letter
+		// (?=\S*[\d]): and at least one number
+		// $: anchored to the end of the string
+
+		//'passwordRegexp' => '^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$',
+		
+
+		// Here you can set your handler to change layout for any controller or action
+		// Tip: you can use this event in any module
+		'on beforeAction'=>function(yii\base\ActionEvent $event) {
+				if ( $event->action->uniqueId == 'user-management/auth/login' )
+				{
+					$event->action->controller->layout = 'loginLayout.php';
+				};
+			},
+	],
+],
+
 ];
